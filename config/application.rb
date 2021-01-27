8d57dcd339cfe44d622c55f29ae577261a4112d3
brandaoplaster <brandaoplaster@gmail.com>
2021-01-26 22:38:34 -0300

    chore: rename project

--- config/application.rb
+++ config/application.rb
@@ -6,7 +6,7 @@ require 'rails/all'
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-module CodeLab27
+module GoTry
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
